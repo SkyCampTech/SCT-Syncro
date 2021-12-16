13bfcc241dc5d6a581c1e85d184c1fdf6820c188
sctcraig <87447024+sctcraig@users.noreply.github.com>
2021-12-16 12:45:54 -0500

    confirmed working, added Activity Logging

--- Remove-MappedDrive.ps1
+++ Remove-MappedDrive.ps1
@@ -10,15 +10,18 @@ Import-Module $env:SyncroModule
 $drive = $drive.ToUpper() + ":"
 
 $currentUser = $env:USERDOMAIN + "\" + $env:USERNAME
-#$currentDrives = Get-PSDrive | Where-Object { $_.Provider -match "FileSystem" -and $_.Name -ne "C" }
+
+
 
 if ($removeAllDrives -match "yes") {
     Write-Host "Removing all mapped drives for $currentUser"
-    net use * /delete /y
+    net use * /delete /yes
+    Display-Alert "We have removed all your mapped drives. Please reboot your computer to finish this update."
     Log-Activity -Message "Removed all mapped drives for $currentUser"
 }
 else {
     Write-Host "Removing Drive $drive for $currentUser"
-    Remove-SmbMapping -LocalPath $drive -Force
+    net use $drive /delete /yes
+    Display-Alert "We have removed the mapped drive $drive. Please reboot your computer to finish this update."
     Log-Activity -Message "Removed Drive $drive for $currentUser"
 }
