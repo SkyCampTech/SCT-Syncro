298ebf64ee80368b98116c3a8e84ad295d45f71c
sctcraig <87447024+sctcraig@users.noreply.github.com>
2021-12-21 12:07:07 -0500

    include check for syncro module and configure env if not there

--- Install-Win10FeatureUpdate.ps1
+++ Install-Win10FeatureUpdate.ps1
@@ -1,6 +1,23 @@
 #credit to drhodes: https://community.syncromsp.com/t/script-to-update-windows-to-version-20h2/127/8
 
-Import-Module $env:SyncroModule
+if ($env:SyncroModule) {
+    Import-Module $env:SyncroModule -WarningAction SilentlyContinue
+}
+else {
+    # Set up $env: Variables and import the syncro module
+    try {
+        $syncroReg = Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\RepairTech\Syncro' -Name shop_subdomain, uuid -ErrorAction Stop
+        $env:RepairTechApiBaseURL = 'syncromsp.com'
+        $env:RepairTechApiSubDomain = $syncroReg.shop_subdomain
+        $env:RepairTechFilePusherPath = 'C:\ProgramData\Syncro\bin\FilePusher.exe'
+        $env:RepairTechUUID = $syncroReg.uuid
+        $env:SyncroModule = "$env:ProgramData\Syncro\bin\module.psm1"
+        Import-Module $env:SyncroModule -WarningAction SilentlyContinue
+    }
+    catch {
+        'Could not find Syncro Module info'
+    }
+}
 
 # Set Variables:
 $targetVersion = "10.0.19044"   #Windows 10 21h2
