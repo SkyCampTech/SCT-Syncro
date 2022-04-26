d5d567584fb531120dbbfd12910ee776392bfaf5
sctcraig <87447024+sctcraig@users.noreply.github.com>
2022-04-26 16:15:51 -0400

    added logic to be able to ignore KFM status

--- Office365_Scripts/Check-OneDriveKnownFolderMove.ps1
+++ Office365_Scripts/Check-OneDriveKnownFolderMove.ps1
@@ -2,35 +2,34 @@
 #make sure to run via Current User in Syncro
 Import-Module $env:SyncroModule
 
+#corresponds to "Ignore OneDrive KFM Status asset custom field"
+if ($noKFM -eq "yes" -or "true") {
+    Write-Host "Known Folder Move not required on this machine. Exiting"
+    exit
+}
+
 $Desktop = (Get-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -name "Desktop").Desktop
 $Pictures = (Get-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -name "My Pictures").'My pictures'
 $Documents = (Get-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -name "Personal").Personal
 
+
+#this whole check section doesn't appear to be working properly; commmitting for now, but need to revisit this
 if ($Desktop -notlike "$($ENV:OneDrive)*") { 
     $KFMDesktop = "Desktop is not set to Onedrive location." 
-}
-else {
-    $KFMDesktop = "Desktop configured correctly." 
 }
 if ($Pictures -notlike "$($ENV:OneDrive)*") { 
     $KFMPictures = "Pictures is not set to Onedrive location." 
 }
-else {
-    $KFMPictures = "Pictures configured correctly." 
-}
 if ($Documents -notlike "$($ENV:OneDrive)*") { 
     $KFMDocuments = "Documents is not set to Onedrive location." 
 }
-else {
-    $KFMDocuments = "Documents configured correctly." 
-}
 
-if (($KFMDesktop -or $KFMPictures -or $KFMDocuments) -match "not set") {
+if ($KFMDesktop -or $KFMPictures -or $KFMDocuments) {
     $rmmBody = @"
 Known Folder Move not configured for $env:username.
-Desktop: $KFMDesktop
-Pictures: $KFMPictures
-Documents: $KFMDocuments
+Desktop: $Desktop
+Pictures: $Pictures
+Documents: $Documents
 "@
 
     Rmm-Alert -Category "Alert" -Body $rmmBody
