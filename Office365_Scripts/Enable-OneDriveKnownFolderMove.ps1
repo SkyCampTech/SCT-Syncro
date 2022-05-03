7ba258d26ed6d8365abc796fde9d3d7b6c1a7537
sctcraig <87447024+sctcraig@users.noreply.github.com>
2022-05-03 12:45:54 -0400

    updated with more verbosity and ignoring noKFM status

--- Office365_Scripts/Enable-OneDriveKnownFolderMove.ps1
+++ Office365_Scripts/Enable-OneDriveKnownFolderMove.ps1
@@ -2,8 +2,15 @@
 #run this as System in Syncro. This script is setting the HKLM settings
 Import-Module $env:SyncroModule
 
+#corresponds to "Ignore OneDrive KFM Status asset custom field"
+if ($noKFM -eq "yes") {
+    Write-Host "Known Folder Move not required on this machine. Exiting"
+    exit
+}
+
 #check to make sure we are able to pull a tenant ID from the Customer's Custom Fields; must be pre set. We do this by API
 if (!($tenantID)) {
+    Write-Host "No Office 365 TEnant ID set for $companyName"
     Rmm-Alert -Category "Alert" -Body "No Office 365 Tenant ID set for $companyName"
     exit 1;
 }
@@ -14,8 +21,7 @@ if (!(Test-Path -Path $regPath)) {
     New-Item -Path $regPath -Force
 }
 
-Write-Host "Enabling Known Folder Move on $nev:ComputerName with Tenant ID: $tenantID"
+Write-Host "Enabling Known Folder Move on $env:ComputerName with Tenant ID: $tenantID for $companyName"
 
 New-ItemProperty -Path $regPath -Name "KFMSilentOptIn" -Value $tenantID -Force
 New-ItemProperty -Path $regPath -Name "KFMSilentOptInWithNotification" -Value "1" -Force
-
