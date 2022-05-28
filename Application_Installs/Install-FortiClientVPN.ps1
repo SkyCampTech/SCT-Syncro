a400bbc4eb7fb00c7e4ce32515af6fc454bee567
sctcraig <87447024+sctcraig@users.noreply.github.com>
2022-05-27 20:53:54 -0400

    initial commit

---
+++ Application_Installs/Install-FortiClientVPN.ps1
@@ -0,0 +1,21 @@
+Import-Module $env:SyncroModule
+
+$tempPath = "C:\ProgramData\SkyCampTech\temp"
+$installerPath = Join-Path $tempPath -ChildPath "FortiClientVPN.exe"
+$fortiClientPath = "C:\Program Files\Fortinet\FortiClient\FortiClient.exe"
+
+Start-Process -FilePath $installerPath -ArgumentList "/quiet /norestart" -Wait
+
+if (!(Test-Path $fortiClientPath)) {
+    Write-Host "FortiClient Not installed; Removing installer from temp and exiting"
+    Remove-Item -Path $installerPath -Force
+    exit 1
+}
+else {
+    #check FortiClient version number
+    $fortiClientVersion = (Get-ItemProperty -Path $fortiClientPath | Select -ExpandProperty VersionInfo).FileVersion
+    Write-Host "Installed FortiClient Version: $fortiClientVersion"
+}
+
+#remove the installer
+Remove-Item -Path $installerPath -Force
