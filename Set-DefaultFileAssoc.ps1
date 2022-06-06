f65dc20eaea6ea439a3339bd9c7705c5e84e5983
sctIsaac <106993513+sctIsaac@users.noreply.github.com>
2022-06-06 14:01:36 -0400

    Added process for HTTP/HTTPS and try catch block for normal process w/ error processing

--- Set-DefaultFileAssoc.ps1
+++ Set-DefaultFileAssoc.ps1
@@ -25,10 +25,25 @@ $current = & $FTAexe get | findstr $fileExtension
 Write-Host "Current Config: $current"
 Write-Host "Changing $fileExtension to use $programName with $programID"
 
-Start-Process -FilePath $FTAexe -ArgumentList "$fileExtension $programID" -Wait
+#If statement for http or https in the file extension
+if ($fileExtension -eq "http" -or $fileExtension -eq "https") { 
+    Start-Process -FilePath $FTAexe -ArgumentList "http $programID" -Wait;
+    Start-Process -FilePath $FTAexe -ArgumentList "https $programID" -Wait;
+    $httpflag = $true; #flag so we don't do the "if" process again.
+}
+
+if ($httpflag) {
+    #try-catch block to process missing period before extension
+    try {
+        Start-Process -FilePath $FTAexe -ArgumentList "$fileExtension $programID" -Wait;
+    }
+    catch {
+        $fileExtension = "." + $fileExtension;
+        Start-Process -FilePath $FTAexe -ArgumentList "$fileExtension $programID" -Wait;
+        Write-Host $_; #error reporting
+    }
+}
 
 $current = & $FTAexe get | findstr $fileExtension
 
 Write-Host "New Config: $current"
-
-#test
