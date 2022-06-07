16d5af63a0a249a3f4bfe4912a81b971552c09a6
sctIsaac <106993513+sctIsaac@users.noreply.github.com>
2022-06-07 10:04:10 -0400

    Update to main portion of program program type and name to set appropriately.

--- Set-DefaultFileAssoc.ps1
+++ Set-DefaultFileAssoc.ps1
@@ -6,6 +6,9 @@ Import-Module $env:SyncroModule
 
 $FTAexe = "c:\programdata\skycamptech\bin\SetUserFTA.exe"
 
+<#SYNCRO: needs to send two variables. 
+$setprogram = the type ex: video, pdf, etc
+$programName = the desired program ex: firefox, adobe, chrome, etc.#>
 #hashtable for each target
 $browserProgIds = @{
     "Firefox" = "FirefoxURL-308046B0AF4A39CB"
@@ -29,6 +32,7 @@ $mailProgIds = @{
 }
 
 #hashtable of extensions/protocols to target when adjusting something
+#CHANGED: added Video for all video format extensions
 $extOrProtocol = @{
     "Browser"  = @{
         "HTTP"  = "http"
@@ -36,61 +40,75 @@ $extOrProtocol = @{
     }
     "PDF"      = ".pdf"
     "Mail"     = "mailto"
-    "MOV"      = ".mov"
-    "MP4"      = ".mp4"
-    "FLV"      = ".flv"
-    "AVI"      = ".avi"
-    "M4A"      = ".m4a"
-    "M4V"      = ".m4v"
-    "WAV"      = ".wav"
+    "Video"    = @{
+        "MOV" = ".mov"
+        "MP4" = ".mp4"
+        "FLV" = ".flv"
+        "AVI" = ".avi"
+        "M4A" = ".m4a"
+        "M4V" = ".m4v"
+        "WAV" = ".wav"
+    }
     "FTP"      = "ftp"
     "WebCal"   = "webcal"
     "Telepone" = "tel"
     "Zoom"     = "ZoomPhoneCall"
 }
 
-$programList = @{
-    "Adobe Reader"   = "AcroExch.Document.DC"
-    "Edge PDF"       = "MSEdgePDF"
-    "VLC Mov"        = "VLC.mov"
-    "Chrome Browser" = "ChromeHTML"
-    "Edge Browser"   = "MSEdgeHTM"
-}
+#CHANGED: programList is unnecessary in this format
+# $programList = @{
+#     "Adobe Reader"   = "AcroExch.Document.DC"
+#     "Edge PDF"       = "MSEdgePDF"
+#     "VLC Mov"        = "VLC.mov"
+#     "Chrome Browser" = "ChromeHTML"
+#     "Edge Browser"   = "MSEdgeHTM"
+# }
 
-#everything from here down should be refactored to account for the hashtables above
-#pull current settings and log it
-#update to the above; assume it's passed by dropdown (Browser, PDF, Mail are the ones we want to start with)
+# everything from here down should be refactored to account for the hashtables above
+# pull current settings and log it
+# update to the above; assume it's passed by dropdown (Browser, PDF, Mail are the ones we want to start with)
 
 #if($fileExtension -notcontains "."){
 #    $fileExtension = "." + $fileExtension
 #}
 
-$programID = $programList[$programName]
+#Sets extensions to change
+$fileExtension = $extOrProtocol[$setProgram];
 
-$current = & $FTAexe get | findstr $fileExtension
-
-Write-Host "Current Config: $current"
-Write-Host "Changing $fileExtension to use $programName with $programID"
-
-#If statement for http or https in the file extension
-if ($fileExtension -eq "http" -or $fileExtension -eq "https") { 
-    Start-Process -FilePath $FTAexe -ArgumentList "http $programID" -Wait;
-    Start-Process -FilePath $FTAexe -ArgumentList "https $programID" -Wait;
-    $httpflag = $true; #flag so we don't do the "if" process again.
-}
-
-if ($httpflag) {
-    #try-catch block to process missing period before extension
-    try {
-        Start-Process -FilePath $FTAexe -ArgumentList "$fileExtension $programID" -Wait;
+# Sets program type
+switch ($setProgram) {
+    "Browser" {
+        $programList = $browserProgIds;
     }
-    catch {
-        $fileExtension = "." + $fileExtension;
-        Start-Process -FilePath $FTAexe -ArgumentList "$fileExtension $programID" -Wait;
-        Write-Host $_; #error reporting
+    "PDF" {
+        $programList = $pdfProgIds
+    }
+    "Mail" {
+        $programList = $mailProgIds
+    }
+    "Video" {
+        $programList = $pdfProgIds
     }
 }
+$programID = $programList[$programName] #programID is the targeted application
 
-$current = & $FTAexe get | findstr $fileExtension
 
-Write-Host "New Config: $current"
+foreach ($this in $fileExtension) {
+    <# $this is the current item #>
+    $current = & $FTAexe get | findstr $this
+
+    Write-Host "Current Config: $current"
+    Write-Host "Changing $this to use $programName with $programID"
+    if ($setProgram -eq "Video") {
+        
+        Start-Process -FilePath $FTAexe -ArgumentList "$fileExtension $programID.$fileExtension" -Wait;
+    }
+    else {
+        Start-Process -FilePath $FTAexe -ArgumentList "$fileExtension $programID" -Wait;
+    }
+    $current = & $FTAexe get | findstr $fileExtension
+
+    Write-Host "New Config: $current"
+}
+
+
