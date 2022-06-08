f193e02e29e704e523ac54cd6bc3ab6f1af6f87e
sctIsaac <106993513+sctIsaac@users.noreply.github.com>
2022-06-08 16:00:13 -0400

    Run as user TODO: clean up code

--- Disable-MSStoreNotifications.ps1
+++ Disable-MSStoreNotifications.ps1
@@ -1,17 +1,24 @@
 Import-Module $env:SyncroModule
 
 <#Path of microsoft store popups#>
-$path = "Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\Microsoft.WindowsStore_8wekyb3d8bbwe!App"
+$path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\Microsoft.WindowsStore_8wekyb3d8bbwe!App"
 if ($MicrosoftStore) {
-    New-item -Path $path
-    New-ItemProperty -Path $path -Name 'Enabled' -Value 0 -PropertyType DWord
-    Write-Host "Microsoft Store ads removed!"
+    try {
+        New-ItemProperty -Path $path -Name "Enabled" -Value 0 -PropertyType DWord -Force 
+        Write-Host "Microsoft Store notifications removed!"
+    }
+    catch {
+        Write-Host "FAILED: Couldn't remove Microsoft Store Notifications"
+    }
+    
 }
 
-<#Path of System Settings popups#>
-$path = "Computer\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\windows.immersivecontrolpanel_cw5n1h2txyewy!microsoft.windows.immersivecontrolpanel"
-if ($Settings) {
-    New-item -Path $path
-    New-ItemProperty -Path $path -Name 'Enabled' -Value 0 -PropertyType DWord
-    Write-Host "System Settings popups removed!"
-}
+# <#Path of System Settings popups#>
+# $path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\windows.immersivecontrolpanel_cw5n1h2txyewy!microsoft.windows.immersivecontrolpanel"
+# if ($Settings) {
+#     If (!(Test-Path $path)) {
+#         New-Item -Path $path -Force | Out-Null
+#     }  
+#     New-ItemProperty -Path $path -Name 'Enabled' -Value 0 -PropertyType DWord -Force 
+#     Write-Host "System Settings popups removed!"
+# }
