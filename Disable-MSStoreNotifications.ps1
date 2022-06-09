91dccb56a71de7b93bea8e4380470be3e11a9742
sctIsaac <106993513+sctIsaac@users.noreply.github.com>
2022-06-09 10:20:39 -0400

    Added settings, newstuffupdates, and tips & Tricks notifications to the list.

--- Disable-MSStoreNotifications.ps1
+++ Disable-MSStoreNotifications.ps1
@@ -1,8 +1,9 @@
 Import-Module $env:SyncroModule
 
-<#Path of microsoft store popups#>
-$path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\Microsoft.WindowsStore_8wekyb3d8bbwe!App"
-if ($MicrosoftStore) {
+
+if ($MicrosoftStore -eq "True") {
+    <#Path of microsoft store popups#>
+    $path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\Microsoft.WindowsStore_8wekyb3d8bbwe!App"
     try {
         New-ItemProperty -Path $path -Name "Enabled" -Value 0 -PropertyType DWord -Force 
         Write-Host "Microsoft Store notifications removed!"
@@ -10,15 +11,39 @@ if ($MicrosoftStore) {
     catch {
         Write-Host "FAILED: Couldn't remove Microsoft Store Notifications"
     }
-    
 }
 
-# <#Path of System Settings popups#>
-# $path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\windows.immersivecontrolpanel_cw5n1h2txyewy!microsoft.windows.immersivecontrolpanel"
-# if ($Settings) {
-#     If (!(Test-Path $path)) {
-#         New-Item -Path $path -Force | Out-Null
-#     }  
-#     New-ItemProperty -Path $path -Name 'Enabled' -Value 0 -PropertyType DWord -Force 
-#     Write-Host "System Settings popups removed!"
-# }
+
+if ($Settings -eq "True") {
+    <#Path of System Settings popups#>
+    $path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\windows.immersivecontrolpanel_cw5n1h2txyewy!microsoft.windows.immersivecontrolpanel"
+    try {
+        New-ItemProperty -Path $path -Name "Enabled" -Value 0 -PropertyType DWord -Force 
+        Write-Host "System Settings notifications removed!"
+    }
+    catch {
+        Write-Host "FAILED: Couldn't remove Microsoft Store Notifications"
+    }
+}
+
+if ($NewStuffUpdates -eq "True") {
+    $path = "\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
+    try {
+        New-ItemProperty -Path $path -Name "SubscribedContent-338389Enabled" -Value 0 -PropertyType DWord -Force 
+        Write-Host "New stuff from update notifications removed!"
+    }
+    catch {
+        Write-Host "FAILED: Couldn't remove Microsoft Store Notifications"
+    }   
+}
+
+if ($TipsAndTricks -eq "True") {
+    $path = "\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
+    try {
+        New-ItemProperty -Path $path -Name "SubscribedContent-310093Enabled" -Value 0 -PropertyType DWord -Force 
+        Write-Host "Tips, Tricks, and Suggestions notifications removed!"
+    }
+    catch {
+        Write-Host "FAILED: Couldn't remove Microsoft Store Notifications"
+    }   
+}
