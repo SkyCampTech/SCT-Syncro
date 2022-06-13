611e0ddf3e3f70539cc6cba009f3502ff8a5c0a4
sctIsaac <106993513+sctIsaac@users.noreply.github.com>
2022-06-13 12:40:57 -0400

    Initial Commit

---
+++ Update-ViaDellCommandUpdates.ps1
@@ -0,0 +1,68 @@
+Import-Module $env:SyncroModule
+
+$MachineModel = Get-WMIObject -class Win32_ComputerSystem | Select-Object -Property "Manufacturer" 
+if ($MachineModel.Value -eq "Dell inc.") {
+    #If check for dell-commmand
+    $choco = 'C:\Program Files\RepairTech\Syncro\kabuto_app_manager\kabuto_patch_manager.exe'
+
+    #Try to upgrade but if that fails do an install
+    try {
+        Start-Process $choco -ArgumentList "upgrade DellCommandUpdate -y" -Wait 
+        Write-Host "Dell Command was updated"
+    }
+    catch {
+        Start-Process $choco -ArgumentList "install DellCommandUpdate -y" -Wait
+        Write-Host "Dell Command was installed"
+    }
+
+    $executablePath = "$env:Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe"
+    $tempFolder = "$env:ProgramData\SkyCampTech\temp"
+
+    Start-Process "$($executablePath)" -ArgumentList "/scan -report=$($tempFolder)" -Wait
+    write-host "Checking for results."
+
+    if (Test-Path -path "$tempFolder\DCUApplicableUpdates.xml") {
+        [xml]$XMLReport = get-content "$tempFolder\DCUApplicableUpdates.xml"
+        $availableUpdates = $XMLReport.updates.update
+     
+        $BIOSUpdates = ($availableUpdates | Where-Object { $_.type -eq "BIOS" }).name.Count
+        $ApplicationUpdates = ($availableUpdates | Where-Object { $_.type -eq "Application" }).name.Count
+        $DriverUpdates = ($availableUpdates | Where-Object { $_.type -eq "Driver" }).name.Count
+        $FirmwareUpdates = ($availableUpdates | Where-Object { $_.type -eq "Firmware" }).name.Count
+        $OtherUpdates = ($availableUpdates | Where-Object { $_.type -eq "Other" }).name.Count
+        $PatchUpdates = ($availableUpdates | Where-Object { $_.type -eq "Patch" }).name.Count
+        $UtilityUpdates = ($availableUpdates | Where-Object { $_.type -eq "Utility" }).name.Count
+        $UrgentUpdates = ($availableUpdates | Where-Object { $_.Urgency -eq "Urgent" }).name.Count
+    
+        #Print Results
+        write-host "Bios Updates: $BIOSUpdates"
+        write-host "Application Updates: $ApplicationUpdates"
+        write-host "Driver Updates: $DriverUpdates"
+        write-host "Firmware Updates: $FirmwareUpdates"
+        write-host "Other Updates: $OtherUpdates"
+        write-host "Patch Updates: $PatchUpdates"
+        write-host "Utility Updates: $UtilityUpdates"
+        write-host "Urgent Updates: $UrgentUpdates"
+    }
+
+    $Result = $BIOSUpdates + $ApplicationUpdates + $DriverUpdates + $FirmwareUpdates + $OtherUpdates + $PatchUpdates + $UtilityUpdates + $UrgentUpdates
+    write-host "Total Updates Available: $Result"
+    if ($Result -gt 0) {
+
+        $OPLogExists = Test-Path "$tempFolder\updateOutput.log"
+        if ($OPLogExists -eq $true) {
+            remove-item "$tempFolder\updateOutput.log" -Force
+        }
+        remove-item "$tempFolder\DCUApplicableUpdates.xml" -Force #Remove XML file
+        write-host "Updating Drivers! This may take a while..."
+        Start-Process "$($executablePath)" -ArgumentList "/applyUpdates -autoSuspendBitLocker=enable -reboot=$($Reboot) -outputLog=$($tempFolder)\updateOutput.log" -Wait
+        Start-Sleep -s 60
+        Get-Content -Path '$tempFolder\updateOutput.log'
+        Log-Activity -Message "Dell Command Updates ran."
+        write-host "Done."
+    }
+}
+else {
+    Write-Host $MachineModel.Value " is not a dell product and can't install Dell Command."
+    exit 1
+}
