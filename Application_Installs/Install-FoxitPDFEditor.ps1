abf72dfc79d83f310f03f5294eaa8c504c706d4c
sctcraig <87447024+sctcraig@users.noreply.github.com>
2022-06-21 13:00:34 -0400

    initial commit (not working)

---
+++ Application_Installs/Install-FoxitPDFEditor.ps1
@@ -0,0 +1,51 @@
+#installs the Foxit PDF Editor (version 11)
+#msi switches: https://kb.foxitsoftware.com/hc/en-us/articles/360040660271--Command-line-Deployments-of-Foxit-PDF-Editor
+Import-Module $env:SyncroModule
+
+$downloadURL = "https://f001.backblazeb2.com/file/SCT-INSTALLERS/Foxit11.msi"
+$tempPath = "$env:ProgramData\SkyCampTech\Temp"
+$foxitInstaller = Join-Path $tempPath -ChildPath "Foxit11.msi"
+$msiexecPath = "C:\Windows\System32\msiexec.exe"
+
+#download installer
+Write-Host "Downloading Installer"
+Start-BitsTransfer -Source $downloadURL -Destination $foxitInstaller
+
+function Install-FoxitWithKeycode {
+    Write-Host "Keycode included; Installing Foxit with Activation Keycode"
+    $installArgs = @"
+    /i $foxitInstaller /aAll keycode="$($keycode)"
+"@
+    Start-Process $msiexecPath -ArgumentList $installArgs -Wait
+
+    $activationPath = "C:\Program Files (x86)\Foxit Software\Foxit PDF Editor\Activation.exe"
+
+    if (Test-Path $activationPath) {
+        Write-Host "Activation Path found; attempting activation"
+        Start-Process -FilePath $activationPath -ArgumentList "-cmdquietinteractive $keycode"
+    }
+    else {
+        Write-Host "Activation Executable Not Found; will require manual activation"
+    }
+    
+}
+
+function Install-FoxitNoKeycode {
+    Write-Host "Installing Foxit with no keycode; will need to manually be activated"
+    $installArgs = @"
+    /i $foxitInstaller /sAll"
+"@
+    Start-Process $msiexecPath -ArgumentList $installArgs -Wait
+    
+}
+
+if ($keycode) {
+    Install-FoxitWithKeycode
+}
+else {
+    Install-FoxitNoKeycode
+}
+
+Write-Host "Sleeping for 30 seconds, then deleting installer"
+Start-Sleep -Seconds 60
+remove-Item -Path $foxitInstaller -Force -Confirm:$false
