7d3ff316dd5383187b35e900ee1184721916fa40
sctcraig <87447024+sctcraig@users.noreply.github.com>
2022-07-08 13:56:19 -0400

    initial commit

---
+++ Application_Installs/Install-MbamEDR.ps1
@@ -0,0 +1,53 @@
+#built based on request from Syncro FB group: https://www.facebook.com/groups/syncromspusers/posts/1461289441009695/
+<#Assumptions:
+-$customerGroup is a Custom Customer Field
+-$manualGroup is provided at runtime if you wish to override the Customer's Group ID
+-installer is provided as a required file under c:\tools; adjust accordingly
+#>
+Import-Module $env:SyncroModule
+
+#check if a group ID is provided
+if (!($customerGroup -or $manualGroup)) {
+    Write-Host "No group ID provided; exiting"
+    exit 1
+}
+
+#if $manualGroup is defined, use that for the Group ID
+if ($manualGroup) {
+    $installGroup = $manualGroup
+}
+
+#check for services - can build this out a little more if needed to check them independently
+if (!((Get-Service -Name "MBAMService") -and (Get-Service -Name "MBEndpointAgent"))) {
+    Write-Host "Services not found; proceeding with install"
+
+    #define variables
+    $installPath = "c:\tools\mbamEDR.msi"
+
+    $mbamArgs = "/i $installPath /quiet GROUP=$installGroup"
+
+    Write-Host "Installing $installPath with $mbamArgs"
+
+    Start-Process -FilePath "C:\Windows\system32\msiexec.exe" -ArgumentList $mbamArgs -Wait
+
+    Start-Sleep -Seconds 30
+
+    #check if services exist now
+    if ((Get-Service -Name "MBAMService") -and (Get-Service -Name "MBEndpointAgent")) {
+        Write-Host "MBAM Services detected"
+
+        #remove installer?
+        Remove-Item -Path $installPath -Confirm:$false
+
+        exit
+    }
+    else {
+        Write-Host "Services still not found; investigate"
+        exit 1
+    }
+
+}
+else {
+    Write-Host "One of the services is already installed; investigate"
+    exit 1
+}
