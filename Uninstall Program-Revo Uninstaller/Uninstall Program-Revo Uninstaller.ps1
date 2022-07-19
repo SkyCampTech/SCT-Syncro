93a3a665ce96abbd6659127ce8df538aed0894af
sctIsaac <106993513+sctIsaac@users.noreply.github.com>
2022-07-19 15:10:52 -0400

    First functional version. Needs name passed and will accept wildcards(*).

--- Uninstall Program-Revo Uninstaller/Uninstall Program-Revo Uninstaller.ps1
+++ Uninstall Program-Revo Uninstaller/Uninstall Program-Revo Uninstaller.ps1
@@ -1,10 +1,9 @@
 Import-Module $env:SyncroModule;
 
-choco install revouninstallerpro -a;
 $revoPath = "C:\ProgramData\SkyCampTech\bin\Revo\x64";
 
-$ProgramLocation = Invoke-Expression -Command "($revoPath)\RevoCmd.exe /m 'Mozilla*' /i"
-Write-Host "Program: $ProgramLocation[0]"
-Write-Host "Program Location: $ProgramLocation[1]"
-$ProgramLocation = start-process -FilePath "($revoPath)\RevoUnPro.exe" -ArgumentList "/mu $ProgramLocation[0] /Path $ProgramLocation[1] /mode advanced /64" -Wait;
+$ProgramLocation = Invoke-Expression -Command "$($revoPath+ "\RevoCmd.exe") /m '$Program' /i"
+Write-Host "Program: $($ProgramLocation[0])"
+Write-Host "Program Location: $($ProgramLocation[1])"
+$ProgramLocation = start-process -FilePath "$($revoPath+"\RevoUnPro.exe")" -ArgumentList "/mu $ProgramLocation[0] /Path $ProgramLocation[1] /mode advanced /64" -Wait;
 Write-Host "Program was deleted"
