4ae6938651d18197658fd825c1518f1bba9ccbe0
sctIsaac <106993513+sctIsaac@users.noreply.github.com>
2022-07-20 08:52:32 -0400

    TODO: add kill function to stop the program from running before it is deleted so it actually deletes.

--- Uninstall Program-Revo Uninstaller/Uninstall Program-Revo Uninstaller.ps1
+++ Uninstall Program-Revo Uninstaller/Uninstall Program-Revo Uninstaller.ps1
@@ -5,5 +5,14 @@ $revoPath = "C:\ProgramData\SkyCampTech\bin\Revo\x64";
 $ProgramLocation = Invoke-Expression -Command "$($revoPath+ "\RevoCmd.exe") /m '$Program' /i"
 Write-Host "Program: $($ProgramLocation[0])"
 Write-Host "Program Location: $($ProgramLocation[1])"
-$ProgramLocation = start-process -FilePath "$($revoPath+"\RevoUnPro.exe")" -ArgumentList "/mu $ProgramLocation[0] /Path $ProgramLocation[1] /mode advanced /64" -Wait;
-Write-Host "Program was deleted"
+
+stop-process -Name "*$Program*" -Force
+
+$ProgramLocation = start-process -FilePath "$($revoPath+"\RevoUnPro.exe")" -ArgumentList "/mu $($ProgramLocation[0]) /Path $($ProgramLocation[1]) /mode advanced /64" -Wait;
+if ($ProgramLocation[0] -eq (Invoke-Expression -Command "$($revoPath+ "\RevoCmd.exe") /m '$Program' /i")[0]) {
+    Write-Host "Program failed to delete."
+    exit 1
+}
+else {
+    Write-Host "Program was deleted"
+}
