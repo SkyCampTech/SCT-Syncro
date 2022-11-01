13533abc202535ea0d60a7884f271c1599ba6eb3
sctcraig <87447024+sctcraig@users.noreply.github.com>
2022-11-01 17:17:58 -0400

    updated naming to Viewer

--- Install-SplashtopViewer.ps1
+++ Install-SplashtopViewer.ps1
@@ -1,6 +1,6 @@
 Import-Module $env:SyncroModule
 
-function Install-SplashtopStreamer {
+function Install-SplashtopViewer {
     Start-Process -FilePath $installerPath -ArgumentList "prevercheck /s /i hidewindow=1" -Wait
 
     if ((Test-Path -Path $folderPath) -and (Test-Path -Path ($folderPath + "\clientoobe.exe"))) {
@@ -34,7 +34,7 @@ try {
         Write-Host "Installed Version: $installedVersion"
         Write-Host "Latest Version: $downloadedVersion"
         Write-Host "Installed version is not latest version; update it"
-        Install-SplashtopStreamer
+        Install-SplashtopViewer
     }
     else {
         Write-Host "Installed Version matches Current Version"
@@ -42,7 +42,7 @@ try {
 }
 catch {
     Write-Host "Splashtop Viewer not installed; Install it now"
-    Install-SplashtopStreamer
+    Install-SplashtopViewer
 }
 
 Write-Host "Removing installer"
