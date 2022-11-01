278f77640969519c5448a0fcd885c581c7514bc2
sctcraig <87447024+sctcraig@users.noreply.github.com>
2022-11-01 16:56:55 -0400

    fixed version checks/updating

--- Install-SplashtopViewer.ps1
+++ Install-SplashtopViewer.ps1
@@ -1,18 +1,12 @@
 Import-Module $env:SyncroModule
 
-$folderPath = "C:\Program Files (x86)\Splashtop\Splashtop Remote\Client for RMM"
-if ((Test-Path -Path $folderPath) -and (Test-Path -Path ($folderPath + "\clientoobe.exe"))) {
-    Write-Host "SplashtopViewer is already installed on device."
-}
-else {
-    $installerPath = "$env:ProgramData\skycamptech\temp\splashtopviewer.exe"
-    $splashtopUri = "https://my.splashtop.com/rmm/win"
-    Invoke-WebRequest -Uri $splashtopUri -OutFile $installerPath
+function Install-SplashtopStreamer {
     Start-Process -FilePath $installerPath -ArgumentList "prevercheck /s /i hidewindow=1" -Wait
 
     if ((Test-Path -Path $folderPath) -and (Test-Path -Path ($folderPath + "\clientoobe.exe"))) {
+        $installedVersion = (Get-Item $viewerExe).VersionInfo.ProductVersion
         Write-Host "SplashtopViewer was installed properly."
-        Log-Activity -Message "Installed Splashtop Viewer"
+        Log-Activity -Message "Installed Splashtop Viewer version $installedVersion"
     }
     else {
         Write-Host "ERROR: SplashtopViewer was NOT installed properly."
@@ -22,4 +16,33 @@ else {
     }
 }
 
+$installerPath = "$env:ProgramData\skycamptech\temp\splashtopviewer.exe"
+$splashtopUri = "https://my.splashtop.com/rmm/win"
+Invoke-WebRequest -Uri $splashtopUri -OutFile $installerPath
+
+$folderPath = "C:\Program Files (x86)\Splashtop\Splashtop Remote\Client for RMM"
+$viewerExe = Join-Path -Path $folderPath -ChildPath "clientoobe.exe"
+
+try {
+    
+    Get-Item $viewerExe -ErrorAction Stop
+    Write-Host "Splashtop Viewer already installed; compare versions"
+    $installedVersion = (Get-Item $viewerExe).VersionInfo.ProductVersion
+    $downloadedVersion = (Get-Item $installerPath).VersionInfo.ProductVersion
+
+    if ($installedVersion -ne $downloadedVersion) {
+        Write-Host "Installed version is not latest version; update it"
+        Install-SplashtopStreamer
+    }
+    else {
+        Write-Host "Installed Version matches Current Version"
+    }
+}
+catch {
+    Write-Host "Splashtop Viewer not installed; Install it now"
+    Install-SplashtopStreamer
+}
+
+Write-Host "Removing installer"
 remove-Item $installerPath -Force -Confirm:$false
+
