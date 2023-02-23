cbd426b773ab53c60ca9e7c25ecf23d968d4971e
sctcraig <87447024+sctcraig@users.noreply.github.com>
2023-02-23 10:17:57 -0500

    correct output

--- Get-ChocoApps.ps1
+++ Get-ChocoApps.ps1
@@ -7,14 +7,14 @@ $apps = Invoke-Expression -Command "choco list --local-only"
 [array]$apps = @()
 $apps = Invoke-Expression -Command "choco list --local-only -r --idonly"
 #filter out the chocolatey extensions and MS KBs that are installed by choco
-$apps = $apps | Where-Object { $_ -notlike "chocolatey*" -and $_ -notlike "KB*" }
+#$apps = $apps | Where-Object { $_ -notlike "chocolatey*" -and $_ -notlike "KB*" }
 
-[array]$appList = @()
+#[array]$appList = @()
 
 #remove version numbers and first/last lines
-for ($i = 0; $i -lt $apps.count - 1; $i++) {
-    $appList += $apps[$i].Split(" ")[0] + "`r`n"
-}
+#for ($i = 0; $i -lt $apps.count - 1; $i++) {
+#    $appList += $apps[$i].Split(" ")[0] + "`r`n"
+#}
 
 #save output to Asset Field
-Set-Asset-Field -Name "Choco Apps" -Value $appList
+Set-Asset-Field -Name "Choco Apps" -Value $apps
