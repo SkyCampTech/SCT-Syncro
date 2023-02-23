adbf5a96874ed1a9b3d135c2f76a348452fa7772
sctcraig <87447024+sctcraig@users.noreply.github.com>
2023-02-23 10:16:27 -0500

    cleanup - use native way to remove versions

--- Get-ChocoApps.ps1
+++ Get-ChocoApps.ps1
@@ -5,7 +5,7 @@ $apps = Invoke-Expression -Command "choco list --local-only"
 
 #do it a second time, just in case; first run can throw in some adds for Pro/Business that we'd have to filter out
 [array]$apps = @()
-$apps = Invoke-Expression -Command "choco list --local-only"
+$apps = Invoke-Expression -Command "choco list --local-only -r --idonly"
 #filter out the chocolatey extensions and MS KBs that are installed by choco
 $apps = $apps | Where-Object { $_ -notlike "chocolatey*" -and $_ -notlike "KB*" }
 
