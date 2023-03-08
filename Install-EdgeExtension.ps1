d23a86192b1ba2862aab1f80b35e63845098accd
sctcraig <87447024+sctcraig@users.noreply.github.com>
2023-03-08 11:17:50 -0500

    initial commit

---
+++ Install-EdgeExtension.ps1
@@ -0,0 +1,38 @@
+Import-Module $env:SyncroModule
+
+#look up extension IDs in the Edge webstore if you want to supply them manually
+
+#if there are extensions we're deploying frequently, add them to hashtable AND the name to the dropdown in the syncro script
+$extensionList = @{
+    "Bitwarden" = "jbkfoedolllekgbhcbcoahefnbanhhlh"
+}
+
+#if an extension ID was provided, set that as extension
+if ($manualExtensionId) {
+    $extension = $manualExtensionId
+}
+#set extension to the extension ID from the associated name in the hashtable
+else {
+    $extension = $extensionList[$extensionName]
+}
+
+Write-Host "Forcing Extension $extension"
+
+#check if no extension was provided
+if (!($extension)) {
+    Write-Host "Extension not found in list and no manual extension ID specified; exiting"
+    exit 1;
+}
+else {
+    #get current extension highest value
+    $string = (Get-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist").Property[-1]
+    
+    #convert to string and add 1
+    [int]$value = [convert]::ToInt32($string) + 1
+
+    #add extension ID with the value above
+    Write-Host "Adding Extension ($extension) to ExtensionInstallForceList in registry with value $value"
+    reg add HKLM\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForceList /v $value /t REG_SZ /d $extension /f
+}
+
+
