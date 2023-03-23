3c2254c2eaf1d4b0f98a28a31359820080a456af
sctcraig <87447024+sctcraig@users.noreply.github.com>
2023-03-23 09:56:59 -0400

    updated with syncro specific stuff

--- Enable-RemoteDesktop.ps1
+++ Enable-RemoteDesktop.ps1
@@ -1,4 +1,7 @@
 #use this script to enable remote desktop on a computer
+Import-Module $env:SyncroModule
 
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+Log-Activity -Message "Enabled Remote Desktop via script"
