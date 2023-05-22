e3160b58c4640247f9a38e7928c10030c9b93167
sctcraig <87447024+sctcraig@users.noreply.github.com>
2023-05-22 17:22:37 -0400

    added removal

--- Remove-WindowsApps/Remove-WindowsApps.ps1
+++ Remove-WindowsApps/Remove-WindowsApps.ps1
@@ -2,7 +2,19 @@
 #this will get a default list from github, but if you set a link to a text file in the Customer Custom Field "Windows App Removal List", it will reference that instead
 Import-Module $env:SyncroModule
 
+$appsToRemove = (Invoke-RestMethod -Uri "https://raw.githubusercontent.com/SkyCampTech/SCT-Syncro/main/Remove-WindowsApps/Default-Apps.txt").Split([Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries)
+
 if (!($customerAppRemovalList)) {
     Write-Host "No Customer App Removal List defined; using default list"
 }
+else {
+    $appsToRemove = (Invoke-RestMethod -Uri $customerAppRemovalList).Split([Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries)
+}
 
+foreach ($app in $appsToRemove) {
+    Write-Host "Trying to remove $app"
+
+    Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -AllUsers
+
+    Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -eq $app } | Remove-AppxProvisionedPackage -Online
+}
