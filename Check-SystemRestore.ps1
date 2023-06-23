40be742a18f08f71bf326ee2815aaaf82dac64c3
sctcraig <87447024+sctcraig@users.noreply.github.com>
2023-06-23 10:36:26 -0400

    date check not working yet

--- Check-SystemRestore.ps1
+++ Check-SystemRestore.ps1
@@ -27,7 +27,25 @@ if (!($result)) {
 }
 else {
     Write-Host "System Restore already enabled with checkpoints"
-    exit 0
+
+    $lastRestorePoint = $result[-1].CreationTime
+    $sevenDaysAgo = (Get-Date).AddDays(-7)
+
+    if ($lastRestorePoint -gt $sevenDaysAgo) {
+        Write-Host "Last Creation Time: $lastRestorePoint"
+        exit 0
+    }
+    else {
+
+        Write-Host "No checkpoints within last 7 days. Creating"
+        $today = Get-Date -Format yyyy-MM-DD
+
+        $description = "Checkpoint via RMM Script - $today"
+
+        Checkpoint-Computer -Description $description -RestorePointType MODIFY_SETTINGS
+
+        Start-Sleep -Seconds 30
+    }
 }
 
 $result = Get-ComputerRestorePoint
