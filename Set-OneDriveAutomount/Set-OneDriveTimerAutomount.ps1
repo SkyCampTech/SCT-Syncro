7a33634863ef1caab3c26703caf30fa0ce46a50d
sctcraig <87447024+sctcraig@users.noreply.github.com>
2023-07-19 13:07:20 -0400

    updated to check to see if it's not the correct vaule

--- Set-OneDriveAutomount/Set-OneDriveTimerAutomount.ps1
+++ Set-OneDriveAutomount/Set-OneDriveTimerAutomount.ps1
@@ -13,6 +13,20 @@ try {
         Write-Host "Timer Automount set to 0"
         exit 0
     }
+    else {
+        Write-Host "Timer Automount not set to 0; updating"
+        Set-ItemProperty -Path $regPath -Name $regName -Type $regType -Value $regValue
+
+        $currentValue = Get-ItemProperty -Path $regPath -Name $regName | Select-Object -ExpandProperty $regName
+        if ($currentValue -eq $regValue) {
+            Write-Host "Timer Automount now set to 0"
+            exit 0
+        }
+        else {
+            Write-Host "Timer Automount not set properly"
+            exit 1
+        }
+    }
 }
 catch {
     Write-Host "Timer Automount not set to 0; updating"
