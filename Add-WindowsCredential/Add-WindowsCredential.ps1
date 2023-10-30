7bc1d51226740c50205b4575eb113229a82937fc
sctcraig <87447024+sctcraig@users.noreply.github.com>
2023-10-30 18:59:45 -0400

    added check to remove existing entry

--- Add-WindowsCredential/Add-WindowsCredential.ps1
+++ Add-WindowsCredential/Add-WindowsCredential.ps1
@@ -1,5 +1,10 @@
 Import-Module $env:SyncroModule
 
+if ($removeExisting -eq "yes") {
+    Write-Host "Removing existing entry for $server"
+    Start-Process -FilePath "cmdkey.exe" -ArgumentList "/delete:$($server)"
+}
+
 if ($username -notcontains "\") {
     Write-Host "Username didn't include server name; adding server so it will work for Azure AD users"
     $username = "$server\$username"
