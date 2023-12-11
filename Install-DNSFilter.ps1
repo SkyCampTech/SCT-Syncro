9cb6726114b10f041623fc5ec1c5c798946238c4
sctcraig <87447024+sctcraig@users.noreply.github.com>
2023-12-11 16:40:22 -0500

    initial commit

---
+++ Install-DNSFilter.ps1
@@ -0,0 +1,39 @@
+#use this script to install DNSFilter
+#need the site secret key set as a Customer Custom Field
+Import-Module $env:SyncroModule
+
+if (!($siteSecretKey)) {
+    Rmm-Alert -Category "DNSFilter" -Body "No DNSFilter Site Secret Key defined for $companyName; Update and re-run the script"
+    exit 1
+}
+
+$dnsFilterInstaller = "C:\ProgramData\SkyCampTech\temp\DNSFilter_Agent_Setup.msi"
+$dnsFilterAgent = "C:\Program Files\DNSFilter Agent\DNSFilter Agent.exe"
+
+if (Test-Path $dnsFilterAgent) {
+    Write-Host "DNSFilter already installed"
+    exit
+}
+else {
+    Write-Host "Installing DNSFilter"
+    Invoke-WebRequest -Uri "https://download.dnsfilter.com/User_Agent/Windows/DNSFilter_Agent_Setup.msi" -OutFile $dnsFilterInstaller
+    $argList = @"
+/qn /i $dnsFilterInstaller NKEY="$siteSecretKey"
+"@
+    Start-Process "C:\Windows\system32\msiexec.exe" -ArgumentList $argList -Wait
+}
+
+Start-Sleep -Seconds 30
+
+if (Test-Path $dnsFilterAgent) {
+    Write-Host "DNSFilter Agent installed successfully"
+    exit
+}
+else {
+    Write-Host "DNSFilter Agent not detected; exiting with Failure"
+    exit 1
+}
+
+Remove-Item -Path $dnsFilterInstaller -Confirm:$false
+
+#throw an RMM alert that can trigger a notice to user?
