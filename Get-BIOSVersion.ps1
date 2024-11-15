f5ea59ea52a0cd23c415148820a58207351541fc
sctcraig <87447024+sctcraig@users.noreply.github.com>
2024-11-15 08:08:51 -0500

    commit

--- Get-BIOSVersion.ps1
+++ Get-BIOSVersion.ps1
@@ -5,3 +5,11 @@ Import-Module $env:SyncroModule
 $biosVersion = (Get-CimInstance -ClassName Win32_BIOS).SMBIOSBIOSVersion
 
 Set-Asset-Field -Name "BIOS Version" -Value $biosVersion
+
+$desiredVersion = @{
+    "Iridium 14"        = "N.1.10CAR01"
+    "Iridium 16"        = "N.1.10CAR01"
+    "OptiPlex 3080"     = "2.26.0"
+    "OptiPlex 7080"     = "1.29.0"
+    "OptiPlex 9030 AIO" = "A21"    
+}
